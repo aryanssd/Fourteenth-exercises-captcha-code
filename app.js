753a2e0058d18ae36f5aef9f63ce6c3cb89fb140
aryanpror8 <aryanjalai8466@gmail.com>
2021-11-28 11:23:22 +0330

    first-pracices-comments

--- app.js
+++ app.js
@@ -1,18 +1,6 @@
-var myAge = 16;
+// some comments in js 
+// how type comments in js
+/* we can write every thing we want for example in the name of god 
+and my name is aryan im 16 and i live in iran in gilan state i love programming i can make web template with html css bootstrap css grid flex box
+and now im learning js */
 
-var myName = "aryan";
-
-var isLogin = true;
-
-alert(myAge);
-
-alert(myName);
-
-alert(isLogin)
-
-
-alert(typeof myAge);
-
-alert(typeof myName);
-
-alert(typeof isLogin)
