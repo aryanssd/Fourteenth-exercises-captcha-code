60b7e2808268eb540dc45322c1ad3608baeedfc9
aryanpror8 <aryanjalai8466@gmail.com>
2021-11-28 12:45:04 +0330

    first-practices-operators-minus

--- app.js
+++ app.js
@@ -4,6 +4,6 @@ var secondNumber = 4
 
 var thirdNumber = 2 
 
-var plusResult = firstNumber + secondNumber// 12 + 4 = 16
+var minusResult = firstNumber - secondNumber// 12 - 4 = 8
 
-alert(plusResult)
+alert(minusResult)
