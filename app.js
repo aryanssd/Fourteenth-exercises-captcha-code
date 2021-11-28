2d701c1e2c1e2d661ccdb3d4a3d2cf95495da66e
aryanpror8 <aryanjalai8466@gmail.com>
2021-11-28 12:10:44 +0330

    first-practices-number-conversion

--- app.js
+++ app.js
@@ -1,3 +1,40 @@
-var userInfo = prompt("enter your name please ...?", "for example aryan");
+/*var userAge = prompt("enter your age please...?", "for example 25");
 
-alert(userInfo);
+var userNumAge = Number(userAge); // the original of numeric conversion syntax.
+
+alert(typeof userNumAge);
+
+output === number
+*/
+
+
+/*var userAge = prompt("enter your age please...?", "for example 25")
+
+var userNumAge = +userAge// the shortcut of numeric conversion syntax.
+
+alert(typeof userNumAge)
+output === number
+*/
+
+
+/*var userAge = prompt("enter your age please...?", "for example 25")
+
+var userNumAge = +userAge
+
+alert(userNumAge)
+
+if user write a string in userAge prompt output === NaN
+
+*/
+
+
+/*var userAge = prompt("enter your age please...?", "for example 25")
+
+var userNumAge = +userAge
+
+alert(userNumAge)
+
+output === userAge ---- number
+
+
+*/
