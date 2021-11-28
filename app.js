a57d794e44c8ee6f2b9296064752b401a42727ee
aryanpror8 <aryanjalai8466@gmail.com>
2021-11-28 17:39:50 +0330

    second-exercise-check-number-is-even

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var userNumber = Number(prompt("please enter your number... :")) 
 
-if (userNumber / 2 == 0){
+if (userNumber % 2 == 0){
     alert("your number is even")
 }else{
     alert("your number is odd")
