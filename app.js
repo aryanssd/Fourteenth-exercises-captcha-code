64781bc78e2b54febb0e1b631b6b0ca48fac47c1
aryanpror8 <aryanjalai8466@gmail.com>
2021-11-28 11:37:11 +0330

    first-practices-prompt

--- app.js
+++ app.js
@@ -1,6 +1,3 @@
-// some comments in js 
-// how type comments in js
-/* we can write every thing we want for example in the name of god 
-and my name is aryan im 16 and i live in iran in gilan state i love programming i can make web template with html css bootstrap css grid flex box
-and now im learning js */
+var userInfo = prompt("enter your name please ...?", "for example aryan");
 
+alert(userInfo);
