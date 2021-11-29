cd351d46769ab46982d4ac51d43be2a648cbe8cd
aryanpror8 <aryanjalai8466@gmail.com>
2021-11-29 18:45:30 +0330

    Eleventh-exercises-average-number-with-function

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 function userNumber() {
-    var inputNumber = Number(prompt("please enter your number..."))
-    var inputNumberAvg = inputNumber % 2 
-    inputNumberAvg === 0 ? alert(" your number is even = " + inputNumber) : alert(" your number is odd = "+ inputNumber)
+    var firstNumber = Number(prompt("please enter first number..."))
+    var secondNumber = Number(prompt("please enter second number..."))
+    var thirdNumber = Number(prompt("please enter third number..."))
+    var allNumber = firstNumber + secondNumber + thirdNumber
+    var numberAvg = allNumber / 3
+    alert("the average of your number is = " + numberAvg)
 }
 
-userNumber()
+userNumber()f
