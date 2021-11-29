beeef9571f2350d0cd2b9adc825f82ac3d5e6c92
aryanpror8 <aryanjalai8466@gmail.com>
2021-11-29 18:01:17 +0330

    first-practices-function

--- app.js
+++ app.js
@@ -1,23 +1,5 @@
-var userScore = Number(prompt("please enter your score..."))
-
-if (userScore === 20){
-    alert("your level is A")
-}else if (userScore === 19){
-    alert("your level is A")
-}else if (userScore === 18){
-    alert("your level is A")
-}else if (userScore === 17){
-    alert("your level is B")
-}else if (userScore === 16){
-    alert("your level is B")
-}else if (userScore === 15){
-    alert("your level is B")
-}else if (userScore === 14){
-    alert("your level is C")
-}else if (userScore === 13){
-    alert("your level is C")
-}else if (userScore === 12){
-    alert("your level is C")
-}else{
-    alert("you have fired :(")
+function showMassage(){
+    alert("hello world")
 }
+
+showMassage()
