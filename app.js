569b803bdbff275e1497170e7f07fe3e37068ff2
aryanpror8 <aryanjalai8466@gmail.com>
2021-11-29 16:16:33 +0330

    first-practices-opposite-!

--- app.js
+++ app.js
@@ -1,11 +1,5 @@
-var num2 = 4
-var num3 = 3
+var myBool1 = true
 
-if (num2 === 3 && num2 * num3 === 12){ 
-    alert("num2 is 3 and there Multiplication is 12")
-} else if (num3 ===  4 && num2 * num3 ===12) {
-    alert("num3 is 4 and there Multiplication is 12")
-} else {
-    alert("no condition is right")
-}
-
+alert("myBool1 = " + myBool1)
+alert("!myBool1 = " + !myBool1)
+alert("!!myBool1 = " + !!myBool1)
