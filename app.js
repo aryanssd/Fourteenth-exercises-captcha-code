d4c9bf561dd75f1932f3d6dc76a3c8c13eb8ac13
aryanpror8 <aryanjalai8466@gmail.com>
2021-11-29 12:48:41 +0330

    first-practices-else-if

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
-var userMinutes = Number(prompt("please enter minutes and get the hour...","more than 60"))
+var num1 = 13
+var num2 = 4
+var num3 = 3
 
-var userMinutesResult = userMinutes / 60
-
-if(userMinutes < 60) {
-    alert("ERROR: \n it can not Conversion to hour")
-}else{
-    alert(userMinutesResult)
+if(num1 === 10) {
+    alert("the number is 10")
 }
 
+else if(num2 * num3 === 11){
+    alert("the number is 11")
+}
 
+else if(num2 * num3 === num1){
+    alert("the number is 12")
+} else{
+    alert("there is no number")
+}
