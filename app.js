d63db7ea07b09a9747cdef00d2b03104feeab963
aryanpror8 <aryanjalai8466@gmail.com>
2021-11-29 16:27:07 +0330

    first-practices-ternary-condition

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
-var myBool1 = true
+var num2 = 4
+var num3 = 3
 
-alert("myBool1 = " + myBool1)
-alert("!myBool1 = " + !myBool1)
-alert("!!myBool1 = " + !!myBool1)
+num2 + num3 === 7 ? alert('yes, plus of this number are 7 :)') : alert('no, plus of this number are not 7 :(')
+
+var num2 = 4
+var num3 = 4
+
+num2 + num3 === 7 ? alert('yes, plus of this number are 7 :)') : alert('no, plus of this number are not 7 :(')
