c42ba85c43ae42a726f24b2e617dca6add95049b
aryanpror8 <aryanjalai8466@gmail.com>
2021-11-29 18:33:50 +0330

    tenth-exercise-odd-or-even-with-functions

--- app.js
+++ app.js
@@ -1,14 +1,7 @@
-function showText (userText){
-    alert("your parameter: " + userText);
+function userNumber() {
+    var inputNumber = Number(prompt("please enter your number..."))
+    var inputNumberAvg = inputNumber % 2 
+    inputNumberAvg === 0 ? alert(" your number is even = " + inputNumber) : alert(" your number is odd = "+ inputNumber)
 }
 
-showText("aryan :)")
-
-// one parameter in function
-
-function showTwoNumbers(firstNumber, secondNumber) {
-    alert("first parameter: " + firstNumber)
-    alert("second parameter: " + secondNumber)
-}
-
-showTwoNumbers(34, 18)//two parameters in function
+userNumber()
