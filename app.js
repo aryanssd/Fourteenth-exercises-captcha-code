1743492b8705dd4a1e9c9e80b2085329d4669876
aryanpror8 <aryanjalai8466@gmail.com>
2021-11-30 16:00:06 +0330

    twelfth-exercise-length-method-final

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-var userName = String(prompt("please enter your user name ...", "At least 3 character please"))
-var userPassword = prompt("please enter you password ...", "At least 8 character please")
+var userName = String(prompt("Enter Your UserName :", "At least 3 character please"))
+var userPassword = prompt("Enter Your Password :", "At least 8 character please")
 
-if (userName.length <= 3 && userPassword.length <= 8){
-    alert("please enter more than 8 character for password")
-    alert("please enter more than 3 character for username")
-}else if (isNaN(userPassword)){
-    alert("password : \nplease enter a valid password ")
-}else{
-    alert("your password and your user name are right you can enter, welcome :)")
+if (userName.length <= 3 || userPassword.length <= 8){
+    alert("UserName must be at least 3 characters and Password must be at least 8 characters !!")
+}
+else if (isNaN(userPassword)){
+    alert("Password : \nEnter a valid password!!")
+}
+else{
+    alert("Your Password and your UserName are true, welcome :)")
 }
